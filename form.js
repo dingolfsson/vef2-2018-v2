1453da32461bede63e0ff1f66609900f2adca93f
dingolfsson <dai5@hi.is>
2018-02-12 13:40:26 +0000

    Updated

--- form.js
+++ form.js
@@ -7,6 +7,22 @@ function form(req, res) {
   res.render('form', { data });
 }
 
-router.get('/', form);
+//router.get('/', form);
+
+router.get('/', (req, res) => {
+ res.send(`
+ <form method="post" action="/post">
+  <input type="text" name="Nafn">
+  <input type="text" name="Netfang">
+  <input type="text" name="Kennitala">
+  <input type="text" name="Fjoldi">
+  <button>Senda</button>
+</form>
+ `);
+})
+
+router.get('/post', (reg, res) => {
+  res.send('test');
+})
 
 module.exports = router;
